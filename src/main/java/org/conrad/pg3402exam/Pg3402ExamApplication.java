22cd3c8415c9bb2c8fceb49593dadb4ab58d5dd6
conradsandaunet <csandaunet03@gmail.com>
2026-08-27 09:28:45 +0200

    Initial Spring Boot project

---
+++ src/main/java/org/conrad/pg3402exam/Pg3402ExamApplication.java
@@ -0,0 +1,13 @@
+package org.conrad.pg3402exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pg3402ExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pg3402ExamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/conrad/pg3402exam/Pg3402ExamApplicationTests.java
@@ -0,0 +1,13 @@
+package org.conrad.pg3402exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pg3402ExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
