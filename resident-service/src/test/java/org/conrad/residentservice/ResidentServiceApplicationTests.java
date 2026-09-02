c19199fc912235425341fa235e9bba8228d0aa7c
conradsandaunet <csandaunet03@gmail.com>
2026-09-02 13:12:43 +0200

    Refactored into Multi-module monorepo

---
+++ resident-service/src/test/java/org/conrad/residentservice/ResidentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.conrad.residentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResidentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
