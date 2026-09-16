691ce6ebc4ef8c4fc483e174fef2ac1fcc33679d
markusnilsen <makizer@gmail.com>
2026-09-16 15:24:55 +0200

    added swagger ui to residentservice

--- resident-service/src/main/java/org/conrad/residentservice/security/SecurityConfig.java
+++ resident-service/src/main/java/org/conrad/residentservice/security/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig {
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/login", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        .requestMatchers("/auth/login", "/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated()
                 )
 
