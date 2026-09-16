79a8d2c014c1e3eb5904506b24a97c5e170893ec
markusnilsen <makizer@gmail.com>
2026-09-16 15:39:51 +0200

    added swagger ui to billing

--- billing-service/src/main/java/org/conrad/billingservice/security/SecurityConfig.java
+++ billing-service/src/main/java/org/conrad/billingservice/security/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/error").permitAll()
+                        .requestMatchers("/error", "/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
